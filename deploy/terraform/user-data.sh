e538d0292b072e59def1bf7fd530a80d7ff77373
Chris Van Law <chris@chrisvanlaw.com>
2021-09-01 13:47:15 -0600

    refactor: remove terraform
    This will be housed in cvanlaw/infrastructure repo instead.

--- deploy/terraform/user-data.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sudo yum update -y
-sudo yum -y install docker
-sudo systemctl start docker
-sudo systemctl enable --now docker
-
-sudo usermod -a -G docker ec2-user
-
-docker run -p 80:80 --pull cvanlaw/chrisvanlawcom:latest
--- deploy/terraform/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_type" {
-  description = "The type of instance to use."
-  default = "t2.micro"
-}
-
-variable "vpc_id" {
-  description = "The id of the VPC in which to place the ec2 instances."
-  default = "vpc-0f0eb790401d5f8a7"
-}
